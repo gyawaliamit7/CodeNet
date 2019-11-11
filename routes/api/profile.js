a008d2c3e106bca646bacf265f4f736e1f0e4726
gyawaliamit7 <gyawaliamit7@gmail.com>
2019-11-11 02:06:50 -0600

    Github API Connected and working

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -194,6 +194,38 @@ router.delete('/', auth, async (req, res) => {
         console.error(error.message);
         res.status(500).send('Server Error');        
     }
-})
+});
+
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri:encodeURI(`https://api.github.com/users/${
+                req.params.username
+            }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+                'githubClientID'
+            )}&client_secret=${config.get(
+                'githubClientSecret'
+            )}`),
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        };
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+      
+            if (response.statusCode !== 200) {
+              return res.status(404).json({ msg: 'No Github profile found' });
+            }
+      
+            res.json(JSON.parse(body));
+          });
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send('Server Error');       
+    }
+});
 
 module.exports = router;
